a059fd741bb06c392a3eda1355e9bd62c81e4f15
Maxime-Fontaine <m.fontaine@hotmail.fr>
2014-10-27 12:51:50 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,17 @@
+import socket
+import os, sys, re
+
+from PyApex.Constantes import *
+from PyApex.AP1000 import AP1000
+from PyApex.AP2050 import AP2050
+from PyApex.AP2040 import AP2040
+
+
+Version = 1.0
+PythonVersion = 3.4
+
+def version():
+    return Version
+
+def python():
+    return PythonVersion
