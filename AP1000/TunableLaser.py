76015c01701f42432706c56daa47dc572ac3f3fa
Maxime Fontaine <Maxime-Fontaine@users.noreply.github.com>
2014-11-09 10:34:13 +0100

    Update TunableLaser.py
    Import modules in methods where they're used
    Uses Send and Receive of Common module
    Uses Connexion.close() instead of sys.exit()

--- AP1000/TunableLaser.py
+++ AP1000/TunableLaser.py
@@ -1,10 +1,7 @@
-import sys, re
 
-from PyApex.Constantes import *
-from PyApex.Errors import ApexError
 
-from math import log10 as log
-import time
+from PyApex.Common import Send, Receive
+
 
 class TunableLaser():
 
@@ -23,107 +20,104 @@ class TunableLaser():
     def __str__(self):
         return "Tunable Laser in slot " + str(self.SlotNumber)
 
-        
-    def Send(self, Command):
-        if not isinstance(Command, str):
-            raise ApexError(AP1000_ERROR_ARGUMENT_TYPE, "Command")
-            sys.exit()
-        try:
-            self.Connexion.send(Command.encode('utf-8'))
-        except:
-            raise ApexError(AP1000_ERROR_BADCOMMAND, Command)
-            sys.exit()
-
-
-    def Receive(self, ByteNumber=1024):
-        if not isinstance(ByteNumber, int):
-            raise ApexError(AP1000_ERROR_ARGUMENT_TYPE, "ByteNumber")
-            sys.exit()
-        try:
-            data = self.Connexion.recv(ByteNumber)
-        except:
-            raise ApexError(AP1000_ERROR_COMMUNICATION, self.Connexion.getsockname()[0])
-            sys.exit()
-        else:
-            return data.decode('utf-8')
-
 
     def GetType(self):
+        from PyApex.Constantes import AP1000_ERROR_SLOT_TYPE_NOT_DEFINED
+        from PyApex.Constantes import SimuTLS_SlotID, AP1000_TLS_CBAND, AP1000_TLS_LBAND
+        from PyApex.Errors import ApexError
+        import re
+        
         if self.Simulation:
             ID = SimuTLS_SlotID
         else:
             Command = "SLT[" + str(self.SlotNumber).zfill(2) + "]:IDN?\n"
-            self.Send(Command)
-            ID = self.Receive()
+            Send(self.Connexion, Command)
+            ID = Receive(self.Connexion)
 
         if re.findall(str(AP1000_TLS_CBAND), ID.split("/")[1]) != []:
             return 0
         elif re.findall(str(AP1000_TLS_CLBAND), ID.split("/")[1]) != []:
             return 2
         else:
+            self.Connexion.close()
             raise ApexError(AP1000_ERROR_SLOT_TYPE_NOT_DEFINED, self.SlotNumber)
-            sys.exit()
             
     
     def ConvertForWriting(self, Power):
+        from PyApex.Constantes import APXXXX_ERROR_ARGUMENT_VALUE, APXXXX_ERROR_VARIABLE_NOT_DEFINED
+        from PyApex.Errors import ApexError
+        from math import log10 as log
+        
         if self.Unit.lower() == "dbm":
             return Power
         elif self.Unit.lower() == "mw":
             try:
                 log(Power)
             except:
-                raise ApexError(AP1000_ERROR_ARGUMENT_VALUE, "Power")
-                sys.exit()
+                self.Connexion.close()
+                raise ApexError(APXXXX_ERROR_ARGUMENT_VALUE, "Power")
             else:
                 return -10 * log(Power/100)
         else:
-            raise ApexError(AP1000_ERROR_VARIABLE_NOT_DEFINED, "self.Unit")
-            sys.exit()
+            self.Connexion.close()
+            raise ApexError(APXXXX_ERROR_VARIABLE_NOT_DEFINED, "self.Unit")
 
 
     def ConvertForReading(self, Power):
+        from PyApex.Constantes import APXXXX_ERROR_VARIABLE_NOT_DEFINED
+        from PyApex.Errors import ApexError
+        
         if self.Unit.lower() == "mw":
             return 10**(Power / 10)
         elif self.Unit.lower() == "dbm":
             return Power
         else:
-            raise ApexError(AP1000_ERROR_VARIABLE_NOT_DEFINED, "self.Unit")
-            sys.exit()
+            self.Connexion.close()
+            raise ApexError(APXXXX_ERROR_VARIABLE_NOT_DEFINED, "self.Unit")
     
     
     def SetPower(self, Power):
+        from PyApex.Constantes import APXXXX_ERROR_ARGUMENT_TYPE, APXXXX_ERROR_ARGUMENT_VALUE
+        from PyApex.Constantes import AP1000_TLS_POWMIN, AP1000_TLS_POWMAX
+        from PyApex.Errors import ApexError
+        
         if not isinstance(Power, (float, int)):
-            raise ApexError(AP1000_ERROR_ARGUMENT_TYPE, "Power")
-            sys.exit()
+            self.Connexion.close()
+            raise ApexError(APXXXX_ERROR_ARGUMENT_TYPE, "Power")
             
         Power = self.ConvertForWriting(Power)
         if Power < AP1000_TLS_POWMIN[self.Type] or Power > AP1000_TLS_POWMAX[self.Type]:
-            raise ApexError(AP1000_ERROR_ARGUMENT_VALUE, "Power")
-            sys.exit()
+            self.Connexion.close()
+            raise ApexError(APXXXX_ERROR_ARGUMENT_VALUE, "Power")
             
         if self.Simulation:
             self.Power = Power
         else:
             Command = "TLS[" + str(self.SlotNumber).zfill(2) + "]:TPDB" + ("%.1f" % Power) + "\n"
-            self.Send(Command)
+            Send(self.Connexion, Command)
             self.Power = Power
     
     
-    def GetPower(self):      
+    def GetPower(self):
+        from PyApex.Constantes import SimuTLS_Power
+        
         if self.Simulation:
             Power = SimuTLS_Power
         else:
             Command = "TLS[" + str(self.SlotNumber).zfill(2) + "]:TPDB?\n"
-            self.Send(Command)
-            Power = self.Receive()
+            Send(self.Connexion, Command)
+            Power = Receive(self.Connexion)
         
         return self.ConvertForReading(float(Power[:-1]))
     
     
     def SetUnit(self, Unit):
+        from PyApex.Constantes import APXXXX_ERROR_ARGUMENT_TYPE
+        from PyApex.Errors import ApexError
+        
         if not isinstance(Unit, str):
-            raise ApexError(AP1000_ERROR_ARGUMENT_TYPE, "Unit")
-            sys.exit()
+            self.Connexion.close()
+            raise ApexError(APXXXX_ERROR_ARGUMENT_TYPE, "Unit")
         
         if Unit.lower() in self.ValidUnits:
             self.Unit = Unit
@@ -134,17 +128,19 @@ class TunableLaser():
 
 
     def On(self):
+        from time import sleep
+        
         if not self.Simulation:
             Command = "TLS[" + str(self.SlotNumber).zfill(2) + "]:L1\n"
-            self.Send(Command)
+            Send(self.Connexion, Command)
         self.Status = "ON"
-        time.sleep(5)
+        sleep(5)
 
 
     def Off(self):
         if not self.Simulation:
             Command = "TLS[" + str(self.SlotNumber).zfill(2) + "]:L0\n"
-            self.Send(Command)
+            Send(self.Connexion, Command)
         self.Status = "OFF"
 
 
@@ -153,46 +149,55 @@ class TunableLaser():
         
         
     def SetWavelength(self, Wavelength):
+        from PyApex.Constantes import APXXXX_ERROR_ARGUMENT_TYPE, APXXXX_ERROR_ARGUMENT_VALUE
+        from PyApex.Constantes import AP1000_TLS_WLMIN, AP1000_TLS_WLMAX
+        from PyApex.Errors import ApexError
+        
         if not isinstance(Wavelength, (float, int)):
-            raise ApexError(AP1000_ERROR_ARGUMENT_TYPE, "Wavelength")
-            sys.exit()
+            self.Connexion.close()
+            raise ApexError(APXXXX_ERROR_ARGUMENT_TYPE, "Wavelength")
         if Wavelength < AP1000_TLS_WLMIN[self.Type] or Wavelength > AP1000_TLS_WLMAX[self.Type]:
-            raise ApexError(AP1000_ERROR_ARGUMENT_VALUE, "Wavelength")
-            sys.exit()
+            self.Connexion.close()
+            raise ApexError(APXXXX_ERROR_ARGUMENT_VALUE, "Wavelength")
         
         if not self.Simulation:
             Command = "TLS[" + str(self.SlotNumber).zfill(2) + "]:TWL" + ("%4.3f" % Wavelength).zfill(8) + "\n"
-            self.Send(Command)
+            Send(self.Connexion, Command)
 
         self.Wavelength = Wavelength
     
     
     def GetWavelength(self):
+        from PyApex.Constantes import SimuTLS_Wavelength
+        
         if self.Simulation:
             Wavelength = SimuTLS_Wavelength
         else:
             Command = "TLS[" + str(self.SlotNumber).zfill(2) + "]:TWL?\n"
-            self.Send(Command)
-            Wavelength = self.Receive()
+            Send(self.Connexion, Command)
+            Wavelength = Receive(self.Connexion)
             
         return float(Wavelength[:-1])
     
     
     def SetFrequency(self, Frequency):
+        from PyApex.Constantes import APXXXX_ERROR_ARGUMENT_TYPE, APXXXX_ERROR_ARGUMENT_VALUE
+        from PyApex.Constantes import VACCUM_LIGHT_SPEED
+        from PyApex.Errors import ApexError
+        
         if not isinstance(Frequency, (float, int)):
-            raise ApexError(AP1000_ERROR_ARGUMENT_TYPE, "Frequency")
-            sys.exit()
+            self.Connexion.close()
+            raise ApexError(APXXXX_ERROR_ARGUMENT_TYPE, "Frequency")
             
         if Frequency > 0:
             self.SetWavelength(VACCUM_LIGHT_SPEED / Frequency)
         else:
-            raise ApexError(AP1000_ERROR_ARGUMENT_VALUE, "Frequency")
+            self.Connexion.close()
+            raise ApexError(APXXXX_ERROR_ARGUMENT_VALUE, "Frequency")
     
             
     def GetFrequency(self):
-        if not isinstance(ChNumber, int):
-            raise ApexError(AP1000_ERROR_ARGUMENT_TYPE, "ChNumber")
-            sys.exit()
+        from PyApex.Constantes import VACCUM_LIGHT_SPEED
             
         Wavelength = self.GetWavelength()
         return VACCUM_LIGHT_SPEED / Wavelength
