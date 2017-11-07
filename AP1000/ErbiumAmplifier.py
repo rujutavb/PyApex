da1fd6d77fad7e85bb78e2dc91364846bfbf6ba3
Maxime Fontaine <maxime.fontaine@apex-t.com>
2017-11-07 17:42:54 +0100

    Add some AP337X commands for controlling the On/Off and the automatic modes

--- AP1000/ErbiumAmplifier.py
+++ AP1000/ErbiumAmplifier.py
@@ -13,12 +13,17 @@ class ErbiumAmplifier():
         self.Connexion = Equipment.Connexion
         self.Simulation = Simulation
         self.SlotNumber = SlotNumber
+        self.Status = "OFF"
         self.Type = self.GetType()
         self.Unit = "dBm"
+        self.GainUnit = "dB"
         self.Wavelength = 1550
         self.Amplification = 0
         self.IPump = 0
         self.ValidUnits = ["dbm", "mw"]
+        self.ValidGainUnits = ["db", "%"]
+        self.Mode = 0
+        self.Setpoint = [0.0, 0.0]
 
 
     def __str__(self):
@@ -82,45 +87,64 @@ class ErbiumAmplifier():
             raise ApexError(AP1000_ERROR_SLOT_TYPE_NOT_DEFINED, self.SlotNumber)
 
 
-    def ConvertForWriting(self, Power):
+    def ConvertForWriting(self, Value, Type = "power"):
         '''
         Internal use only
-        Convert a dBm power in mW or a mW power in dBm
+        Type is the type of value to convert:
+        If Type is:
+            - "power" or 1, the function converts a power in dBm
+            - "gain" or 2, the function converts a gain in dB
+        Value is the value to convert
         '''
         from PyApex.Constantes import APXXXX_ERROR_ARGUMENT_VALUE, APXXXX_ERROR_VARIABLE_NOT_DEFINED
         from PyApex.Errors import ApexError
         from math import log10 as log
         
-        if self.Unit.lower() == "dbm":
-            return Power
-        elif self.Unit.lower() == "mw":
+        Type = str(Type).lower()
+        if Type == "gain" or Type == 2:
+            Type = 2
+        else:
+            Type = 1
+            
+        if (self.Unit.lower() == "dbm" and Type == 1) or (self.GainUnit.lower() == "db" and Type == 2):
+            return Value
+        else:
             try:
-                log(Power)
+                log(Value)
             except:
                 self.Connexion.close()
-                raise ApexError(APXXXX_ERROR_ARGUMENT_VALUE, "Power")
+                raise ApexError(APXXXX_ERROR_ARGUMENT_VALUE, "Value")
             else:
-                return -10 * log(Power/100)
-        else:
-            self.Connexion.close()
-            raise ApexError(APXXXX_ERROR_VARIABLE_NOT_DEFINED, "self.Unit")
+                if Type == 2:
+                    Value /= 100.0
+                return 10.0 * log(Value/10.0)
 
 
-    def ConvertForReading(self, Power):
+    def ConvertForReading(self, Value, Type = "power"):
         '''
         Internal use only
-        Convert a dBm power in mW or a mW power in dBm
+        Type is the type of value to convert:
+        If Type is:
+            - "power" or 1, the function converts a power in mW if needed
+            - "gain" or 2, the function converts a gain in % if needed
+        Value is the value to convert
         '''
         from PyApex.Constantes import APXXXX_ERROR_VARIABLE_NOT_DEFINED
         from PyApex.Errors import ApexError
         
-        if self.Unit.lower() == "mw":
-            return 10**(Power / 10)
-        elif self.Unit.lower() == "dbm":
-            return Power
+        Type = str(Type).lower()
+        if Type == "gain" or Type == 2:
+            Type = 2
         else:
-            self.Connexion.close()
-            raise ApexError(APXXXX_ERROR_VARIABLE_NOT_DEFINED, "self.Unit")
+            Type = 1
+        
+        if (self.Unit.lower() == "mw" and Type == 1) or (self.GainUnit.lower() == "%" and Type == 2):
+            Value = 10.0**(Value / 10.0)
+            if Type == 2:
+                Value *= 100.0
+            return Value
+        else:
+            return Value
 
 
     def GetInVoltage(self):
@@ -155,7 +179,38 @@ class ErbiumAmplifier():
             OutVoltage = Receive(self.Connexion)
         
         return int(OutVoltage[:-1])
+    
+    
+    def On(self):
+        '''
+        Switch on the pump laser of the EFA equipment
+        Waits 0.2 second after switching on
+        '''
+        from time import sleep
+        
+        if not self.Simulation:
+            Command = "AMP[" + str(self.SlotNumber).zfill(2) + "]:L1\n"
+            Send(self.Connexion, Command)
+        self.Status = "ON"
+        sleep(0.2)
 
+    
+    def Off(self):
+        '''
+        Switch off the pump laser of the EFA equipment
+        '''
+        if not self.Simulation:
+            Command = "AMP[" + str(self.SlotNumber).zfill(2) + "]:L0\n"
+            Send(self.Connexion, Command)
+        self.Status = "OFF"
+        
+        
+    def GetStatus(self):
+        '''
+        Returns the status ("ON" or "OFF") of the TLS equipment
+        '''
+        return self.Status
+        
 
     def SetIPump(self, IPump):
         '''
@@ -163,7 +218,7 @@ class ErbiumAmplifier():
         IPump is expressed in mA
         '''
         from PyApex.Constantes import APXXXX_ERROR_ARGUMENT_TYPE, APXXXX_ERROR_ARGUMENT_VALUE
-        from PyAPex.Constantes import AP1000_EFA_IPMAX
+        from PyApex.Constantes import AP1000_EFA_IPMAX
         from PyApex.Errors import ApexError
         
         try:
@@ -221,7 +276,7 @@ class ErbiumAmplifier():
 
     def SetUnit(self, Unit):
         '''
-        Set the power unit of the EFA equipment
+        Sets the power unit of the EFA equipment
         Unit is a string which could be "dBm" for logaritmic or "mW" for linear power
         '''
         from PyApex.Constantes import APXXXX_ERROR_ARGUMENT_TYPE
@@ -235,11 +290,220 @@ class ErbiumAmplifier():
         else:
             if Unit.lower() in self.ValidUnits:
                 self.Unit = Unit
-
-
+                
+                
     def GetUnit(self):
         '''
-        Get power unit of the EFA equipment
+        Gets power unit of the EFA equipment
         The return unit is a string
         '''
         return self.Unit
+        
+        
+    def SetUnitGain(self, Unit):
+        '''
+        Sets the gain unit of the EFA equipment
+        Unit is a string which could be "dB" for logaritmic or "%" for linear gain
+        '''
+        from PyApex.Constantes import APXXXX_ERROR_ARGUMENT_TYPE
+        from PyApex.Errors import ApexError
+        
+        try:
+            Unit = str(Unit)
+        except:
+            self.Connexion.close()
+            raise ApexError(APXXXX_ERROR_ARGUMENT_TYPE, "Unit")
+        else:
+            if Unit.lower() in self.ValidGainUnits:
+                self.GainUnit = Unit
+
+                
+    def GetGainUnit(self):
+        '''
+        Gets gain unit of the EFA equipment
+        The return unit is a string
+        '''
+        return self.GainUnit
+   
+
+    def SetMode(self, Mode = 0, SetPoint = None):
+        '''
+        Sets the working mode of the EFA equipment
+        Mode can be an integer or a string and represents the working mode
+        of the EFA equipment
+        If Mode is :
+            - "manual" or 0, EFA is running in manual mode (default)
+            - "power" or 1, EFA is running in constant output power mode
+            - "gain" or 2, EFA is running in constant gain mode
+        SetPoint represents the set point of the automatic mode.
+        If no set point is given, the previous one is used.
+        If Mode = "power", SetPoint is expressed in the unit defined by the GetUnit() method
+        If Mode = "gain", SetPoint is expressed in the unit defined by the GetGainUnit() method
+        If Mode = "manual", the SetPoint value has no effect
+        '''
+        from PyApex.Constantes import APXXXX_ERROR_ARGUMENT_TYPE
+        from PyApex.Errors import ApexError
+        
+        Command = "AMP[" + str(self.SlotNumber).zfill(2) + "]:"
+        if isinstance(Mode, str):
+            if Mode.lower() == "power":
+                Command += "AUTOPOWER"
+                self.Mode = 1
+            elif Mode.lower() == "gain":
+                Command += "AUTOGAIN"
+                self.Mode = 2
+            else:
+                Command += "MANUAL"
+                self.Mode = 0
+        else:
+            if Mode == 1:
+                Command += "AUTOPOWER"
+                self.Mode = 1
+            elif Mode == 2:
+                Command += "AUTOGAIN"
+                self.Mode = 2
+            else:
+                Command += "MANUAL"
+                self.Mode = 0
+                
+        if SetPoint != None and self.Mode != 0:
+            if not isinstance(SetPoint, (int, float)):
+                self.Connexion.close()
+                raise ApexError(APXXXX_ERROR_ARGUMENT_TYPE, "SetPoint")
+            else:
+                self.Setpoint[self.Mode - 1] = self.ConvertForWriting(SetPoint, self.Mode)
+        
+        if self.Mode == 1 or self.Mode == 2:
+            Command += ("%.1f" % self.Setpoint[self.Mode - 1])
+        Command += "\n"
+            
+        if not self.Simulation:
+            Send(self.Connexion, Command)
+    
+    
+    def GetMode(self):
+        '''
+        Gets the working mode of the EFA equipment
+        '''
+        
+        Command = "AMP[" + str(self.SlotNumber).zfill(2) + "]:MODE?\n"
+            
+        if not self.Simulation:
+            Send(self.Connexion, Command)
+            Mode = Receive(self.Connexion)
+        
+        try:
+            Mode = int(Mode)
+            self.Mode = Mode
+        except:
+            pass
+        
+        if self.Mode == -1:
+            return "off"
+        elif self.Mode == 0:
+            return "manual"
+        elif self.Mode == 1:
+            return "constant power"
+        elif self.Mode == 2:
+            return "constant gain"
+        else:
+            return "unknown mode"
+    
+    
+    def SetPower(self, Power):
+        '''
+        Sets the power of the constant output power mode and sets the EFA equipment
+        in the constant output power mode
+        SetPoint is a value expressed in the unit defined by the GetUnit() method 
+        '''
+        from PyApex.Constantes import APXXXX_ERROR_ARGUMENT_TYPE
+        from PyApex.Errors import ApexError
+        
+        try:
+            Power = float(Power)
+        except:
+            self.Connexion.close()
+            raise ApexError(APXXXX_ERROR_ARGUMENT_TYPE, "Power")
+        else:
+            self.Setpoint[0] = self.ConvertForWriting(Power, "power")
+            
+            
+            Command = "AMP[" + str(self.SlotNumber).zfill(2) + "]:"
+            Command += "AUTOPOWER" + ("%.1f" % self.Setpoint[0]) + "\n"
+            
+            if not self.Simulation:
+                Send(self.Connexion, Command)
+    
+    
+    def GetPower(self):
+        '''
+        Gets the set point power of the constant output power mode
+        The value is expressed in the unit defined by the GetUnit() method
+        
+        '''
+        Command = "AMP[" + str(self.SlotNumber).zfill(2) + "]:AUTOPOWER?\n"
+            
+        if not self.Simulation:
+            Send(self.Connexion, Command)
+            Power = Receive(self.Connexion)
+            
+            if "no set point" in str(Power).lower():
+                return "no set point"
+        
+        try:
+            Power = float(Power)
+            self.Setpoint[0] = Power
+        except:
+            pass
+        
+        return self.ConvertForReading(self.Setpoint[0], "power")
+    
+    
+    def SetGain(self, Gain):
+        '''
+        Sets the gain of the constant gain mode and sets the EFA equipment
+        in the constant gain mode
+        SetPoint is a value expressed in the unit defined by the GetGainUnit() method 
+        '''
+        from PyApex.Constantes import APXXXX_ERROR_ARGUMENT_TYPE
+        from PyApex.Errors import ApexError
+        
+        try:
+            Gain = float(Gain)
+        except:
+            self.Connexion.close()
+            raise ApexError(APXXXX_ERROR_ARGUMENT_TYPE, "Gain")
+        else:
+            self.Setpoint[1] = self.ConvertForWriting(Gain, "gain")
+            
+            
+            Command = "AMP[" + str(self.SlotNumber).zfill(2) + "]:"
+            Command += "AUTOGAIN" + ("%.1f" % self.Setpoint[1]) + "\n"
+            
+            if not self.Simulation:
+                Send(self.Connexion, Command)
+    
+    
+    def GetGain(self):
+        '''
+        Gets the set point gain of the constant gain mode
+        The value is expressed in the unit defined by the GetGainUnit() method
+        
+        '''
+        Command = "AMP[" + str(self.SlotNumber).zfill(2) + "]:AUTOGAIN?\n"
+            
+        if not self.Simulation:
+            Send(self.Connexion, Command)
+            Gain = Receive(self.Connexion)
+            
+            if "no set point" in str(Gain).lower():
+                return "no set point"
+        
+        try:
+            Gain = float(Gain)
+            self.Setpoint[1] = Gain
+        except:
+            pass
+        
+        return self.ConvertForReading(self.Setpoint[1], "gain")
+
